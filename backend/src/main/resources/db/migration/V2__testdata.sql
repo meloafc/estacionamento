f8624f7c40dab8821f85b7c216bf35c6badc426d
Alex Fabiano <afabiano15@gmail.com>
2019-04-20 22:12:33 -0400

    criando script de iniciação de dados

---
+++ backend/src/main/resources/db/migration/V2__testdata.sql
@@ -0,0 +1,55 @@
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(0, '08:00:00', '18:00:00', 2.5);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(1, '08:00:00', '18:00:00', 2.5);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(2, '08:00:00', '11:59:00', 2);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(2, '12:00:00', '18:00:00', 3);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(3, '08:00:00', '11:59:00', 2);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(3, '12:00:00', '18:00:00', 3);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(4, '08:00:00', '11:59:00', 2);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(4, '12:00:00', '18:00:00', 3);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(5, '08:00:00', '11:59:00', 2);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(5, '12:00:00', '18:00:00', 3);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(6, '08:00:00', '11:59:00', 2);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(6, '12:00:00', '18:00:00', 3);
+INSERT INTO horarios (dia_da_semana, hora_inicial, hora_final, valor) VALUES(7, '08:00:00', '18:00:00', 2.5);
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-15 08:09:07.000', '2019-04-15 11:59:00.000', 0, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-15 08:39:00.000', '2019-04-15 10:41:00.000', 0, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-15 08:40:59.000', '2019-04-15 15:59:00.000', 0, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-15 09:59:32.000', '2019-04-15 12:59:00.000', 0, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-15 10:10:25.000', '2019-04-15 18:59:00.000', 0, 'onix', 'NFB-7297');
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-16 08:09:07.000', '2019-04-16 11:59:00.000', 0, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-16 08:39:00.000', '2019-04-16 10:41:00.000', 0, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-16 08:40:59.000', '2019-04-16 15:59:00.000', 0, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-16 09:59:32.000', '2019-04-16 12:59:00.000', 0, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-16 10:10:25.000', '2019-04-16 18:59:00.000', 0, 'onix', 'NFB-7297');
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-17 08:09:07.000', '2019-04-17 11:59:00.000', 0, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-17 08:39:00.000', '2019-04-17 10:41:00.000', 0, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-17 08:40:59.000', '2019-04-17 15:59:00.000', 0, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-17 09:59:32.000', '2019-04-17 12:59:00.000', 0, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-17 10:10:25.000', '2019-04-17 18:59:00.000', 0, 'onix', 'NFB-7297');
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-18 08:09:07.000', '2019-04-18 11:59:00.000', 0, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-18 08:39:00.000', '2019-04-18 10:41:00.000', 0, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-18 08:40:59.000', '2019-04-18 15:59:00.000', 0, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-18 09:59:32.000', '2019-04-18 12:59:00.000', 0, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-18 10:10:25.000', '2019-04-18 18:59:00.000', 0, 'onix', 'NFB-7297');
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-19 08:09:07.000', '2019-04-19 11:59:00.000', 1, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-19 08:39:00.000', '2019-04-19 10:41:00.000', 1, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-19 08:40:59.000', '2019-04-19 15:59:00.000', 1, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-19 09:59:32.000', '2019-04-19 12:59:00.000', 1, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-19 10:10:25.000', '2019-04-19 18:59:00.000', 1, 'onix', 'NFB-7297');
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-20 08:09:07.000', '2019-04-20 11:59:00.000', 0, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-20 08:39:00.000', '2019-04-20 10:41:00.000', 0, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-20 08:40:59.000', '2019-04-20 15:59:00.000', 0, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-20 09:59:32.000', '2019-04-20 12:59:00.000', 0, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-20 10:10:25.000', '2019-04-20 18:59:00.000', 0, 'onix', 'NFB-7297');
+
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('azul', '2019-04-21 08:09:07.000', '2019-04-21 11:59:00.000', 0, 'fox', 'CVO-7010');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('vermelho', '2019-04-21 08:39:00.000', '2019-04-21 10:41:00.000', 0, 'uno', 'MZV-6549');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('branco', '2019-04-21 08:40:59.000', '2019-04-21 15:59:00.000', 0, 'palio', 'NAZ-8575');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('preto', '2019-04-21 09:59:32.000', '2019-04-21 12:59:00.000', 0, 'celta', 'KAI-8969');
+INSERT INTO movimentos (cor, data_inicial, data_final, feriado, modelo, placa) VALUES('cinza', '2019-04-21 10:10:25.000', '2019-04-21 18:59:00.000', 0, 'onix', 'NFB-7297');
